c8526d4ce1a5f3738576b188a8da4e4400d06ec3
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2021-11-25 01:01:54 -0500

    fo

--- Client.cpp
+++ Client.cpp
@@ -6,19 +6,99 @@
 #include <time.h>
 
 using namespace Sync;
+bool flag = true;
+
+// This thread handles the connection to the server
+class ClientThread : public Thread
+{
+private:
+	// Reference to our connected socket
+	Socket &socket;
+
+	// Data to send to server
+	ByteArray data;
+	std::string data_str;
+
+public:
+	ClientThread(Socket &socket)
+		: socket(socket)
+	{
+	}
+
+	~ClientThread()
+	{
+	}
+
+	virtual long ThreadMain()
+	{
+		bool serverNotReady = false;
+		try
+		{
+			int result = socket.Open();
+		}
+		catch (...)
+		{ //check if server is running
+			std::cout << "Need to run server first!" << std::endl;
+			serverNotReady = true;
+			flag = false;
+		}
+		while (1)
+		{
+			if (serverNotReady)
+			{
+				break;
+			}
+			std::cout << "Please input your data (type 'done' to exit): "<< std::endl;
+			std::cout.flush();
+
+			// Get the data
+			std::getline(std::cin, data_str);
+			data = ByteArray(data_str);
+
+			// Write to the server
+			socket.Write(data);
+			if (data_str == "done")
+			{
+				flag = false;
+				break;
+			}
+			// Get the response
+			socket.Read(data);
+			data_str = data.ToString();
+			std::cout << "SERVER RESPONSE: " << data_str << std::endl;
+			if (data_str == "kill")
+			{
+				flag = false;
+				break;
+			}
+		}
+		std::cout << "Ending client" << std::endl;
+
+		return 0;
+	}
+};
 
 int main(void)
 {
-	// Welcome the user 
-	std::cout << "SE3313 Lab 3 Client" << std::endl;
+	// Welcome the user
+	std::cout << "Hi, I'm a client" << std::endl;
 
 	// Create our socket
-	Socket socket("127.0.0.1", 3000);
-	//To write to socket and read from socket. You may use ByteArray 
-	//socket.Write(data);
-	//socket.Read(data);
-
-	socket.Close();
+	Socket socket("127.0.0.1", 1000);
+	ClientThread clientThread(socket);
+	while (flag)
+	{
+		sleep(1);
+	}
+	try
+	{
+		socket.Close();
+		std::cout << "Socket closed" << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Ending" << std::endl;
+	}
 
 	return 0;
 }
